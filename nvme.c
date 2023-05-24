openSUSE/nvme-cli

commit 13b0b73b6f0fb7d74c4d61166789d71ac201adc7

fabrics: Add old nbft compat commands with backwards compatibilty
In order to keep existing users of the interface happy, add the old nbft
commands back. Also make sure we do not change the behavior. That is
'nvme connect-all' defaults to --nobft, and 'nvme connect-nbft' is the
same as 'nvme connect-all --nbft'.

This is necessary because we dropped the first implementation and
replaced with the upstream version.

Signed-off-by: Daniel Wagner <dwagner@suse.de>

nvme.c
@@ -9152,13 +9152,13 @@ static int show_topology_cmd(int argc, char **argv, struct command *command, str
 static int discover_cmd(int argc, char **argv, struct command *command, struct plugin *plugin)
 {
 	const char *desc = "Send Get Log Page request to Discovery Controller.";
-	return nvmf_discover(desc, argc, argv, false);
+	return nvmf_discover(desc, argc, argv, false, false);
 }
 
 static int connect_all_cmd(int argc, char **argv, struct command *command, struct plugin *plugin)
 {
 	const char *desc = "Discover NVMeoF subsystems and connect to them";
-	return nvmf_discover(desc, argc, argv, true);
+	return nvmf_discover(desc, argc, argv, true, false);
 }
 
 static int connect_cmd(int argc, char **argv, struct command *command, struct plugin *plugin)
@@ -9190,6 +9190,35 @@ static int dim_cmd(int argc, char **argv, struct command *command, struct plugin
 {
 	const char *desc = "Send Discovery Information Management command to a Discovery Controller (DC)";
 	return nvmf_dim(desc, argc, argv);
+}
+
+static int connect_nbft_compat_cmd(int argc, char **argv, struct command *command, struct plugin *plugin)
+{
+	const char *desc = "Connect subsystems listed in ACPI NBFT tables";
+
+	return nvmf_discover(desc, argc, argv, true, true);
+}
+
+static int show_nbft_compat_cmd(int argc, char **argv, struct command *command, struct plugin *plugin)
+{
+	char **argv_plugin;
+	int i, err;
+
+	argv_plugin = calloc(sizeof(char *), argc + 1);
+	if (!argv_plugin)
+		return -ENOMEM;
+
+	argv_plugin[0] = "nbft";
+	argv_plugin[1] = "show";
+
+	for (i = 1; i < argc; i++)
+		argv_plugin[1 + i] = argv[i];
+
+	err = handle_plugin(argc, argv_plugin, plugin);
+
+	free(argv_plugin);
+
+	return err;
 }
 
 void register_extension(struct plugin *plugin)

fabrics.c
@@ -689,7 +689,8 @@ static int discover_from_json_config_file(nvme_root_t r, nvme_host_t h,
 	return ret;
 }
 
-int nvmf_discover(const char *desc, int argc, char **argv, bool connect)
+int nvmf_discover(const char *desc, int argc, char **argv, bool connect,
+		  bool nbft_compat_mode)
 {
 	char *subsysnqn = NVME_DISC_SUBSYS_NAME;
 	char *hostnqn = NULL, *hostid = NULL, *hostkey = NULL;
@@ -710,6 +711,11 @@ int nvmf_discover(const char *desc, int argc, char **argv, bool connect)
 	bool json_config = false;
 	bool nbft = false, nonbft = false;
 	char *nbft_path = NBFT_SYSFS_PATH;
+
+	if (nbft_compat_mode)
+		nbft = true;
+	else
+		nonbft = true;
 
 	OPT_ARGS(opts) = {
 		OPT_STRING("device",   'd', "DEV", &device, "use existing discovery controller device"),

fabrics.h
@@ -12,7 +12,8 @@ struct tr_config {
 };
 
 extern nvme_ctrl_t lookup_ctrl(nvme_root_t r, struct tr_config *trcfg);
-extern int nvmf_discover(const char *desc, int argc, char **argv, bool connect);
+extern int nvmf_discover(const char *desc, int argc, char **argv, bool connect,
+			 bool nbft_compat_mode);
 extern int nvmf_connect(const char *desc, int argc, char **argv);
 extern int nvmf_disconnect(const char *desc, int argc, char **argv);
 extern int nvmf_disconnect_all(const char *desc, int argc, char **argv);

nvme-builtin.h
@@ -109,6 +109,8 @@ COMMAND_LIST(
 	ENTRY("show-topology", "Show the topology", show_topology_cmd) \
 	ENTRY("io-mgmt-recv", "I/O Management Receive", io_mgmt_recv)
 	ENTRY("io-mgmt-send", "I/O Management Send", io_mgmt_send)
+	ENTRY("connect-nbft", "Connect subsystems listed in ACPI NBFT tables", connect_nbft_compat_cmd)
+	ENTRY("show-nbft", "Show ACPI NBFT table contents", show_nbft_compat_cmd)
 );
 
 #endif

commit a62916cb6c324556a4e466b29be516239175b37e

fabrics: fix argc in show_nbft_compat_cmd()
Fixes: 13b0b73 ("fabrics: Add old nbft compat commands with backwards compatibilty")
Signed-off-by: Martin Wilck <mwilck@suse.com>

nvme.c
@@ -9214,7 +9214,7 @@ static int show_nbft_compat_cmd(int argc, char **argv, struct command *command, 
 	for (i = 1; i < argc; i++)
 		argv_plugin[1 + i] = argv[i];
 
-	err = handle_plugin(argc, argv_plugin, plugin);
+	err = handle_plugin(argc + 1, argv_plugin, plugin);
 
 	free(argv_plugin);
 