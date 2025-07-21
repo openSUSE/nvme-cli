f4fa5f36c2d994cebd0f43db803e1a951ccdabeb
Nilay Shroff <nilay@linux.ibm.com>
2025-07-21 15:59:11 +0530

    nvme-list: fix verbose JSON output for 'nvme list' command
    The verbose JSON output of the nvme list command is currently incorrect in
    both multipath and non-multipath configurations. Specifically, it prints
    empty Namespaces[] and Paths[] arrays in the wrong places, leading to
    confusion and invalid output. For example, on a system with single NVMe
    disk, signle controller and one namepsace created, 'nvme list --verbose
    --output json' prints the following output:
    
    With multipath disabled:
    
    {
      "Devices":[
        {
          ...
          "Subsystems":[
            {
              ...
              "Controllers":[
                {
                  "Controller":"nvme0",
    	      ...
                  "Namespaces":[
                    {
                      "NameSpace":"nvme0n1",
    		  ...
                    }
                  ],
                  "Paths":[] <---- Incorrct: Path should not be present
                }
              ],
              "Namespaces":[] <-----Incorrect: Namespaces should not be here
            }
          ]
        }
      ]
    }
    
    With multipath enabled, the output changes, but still has misplaced or
    empty fields:
    
    {
      "Devices":[
        {
          ...
          "Subsystems":[
            {
              ...
              "Controllers":[
                {
                  "Controller":"nvme0",
    	      ...
                  "Namespaces":[] <-----Incorrect: Namespaces should not be here
                  "Paths":[
                    {
                      "Path":"nvme0c0n1",
                      "ANAState":"optimized"
                    }
                  ]
                }
              ],
              "Namespaces":[
                {
                  "NameSpace":"nvme0n1",
    	      ...
                }
              ]
            }
          ]
        }
      ]
    }
    
    So as we could see above in both multipath and non-multipath scenarios,
    the JSON formatted output is incorrect.
    
    The existing JSON formatting logic doesn't differentiate between multipath
    and non-multipath configurations. As a result:
    - "Paths" is printed even when multipath is disabled.
    - "Namespaces" appear at incorrect levels in the output tree.
    
    This patch updates the logic for verbose JSON output in nvme list to
    properly reflect the system configuration:
    - When multipath is enabled, each namespace entry includes its associated
      paths and controller attributes.
    - When multipath is disabled, namespaces are shown directly under the
      controller, and the "Paths" array is omitted.
    
    After this fix, JSON formatted output looks as below:
    
    Scenario1:  multipath is enabled
    {
      Devices: [
        {
          ...
          Subsystems: [
            {
              ...
              Namespaces: [
    	    {
    	      "NameSpace":"nvme0n1",
    	      ...
    	      Paths: [
                    {
                      "Path":"nvme0c0n1",
    		  "ANAState":"optimized",
    		  "Controller":"nvme0",
                      ...
    		}
    	      ]
                }
    	  ]
            }
          ]
        }
      ]
    }
    
    Scenario2: multipath is disabled
    {
      Devices: [
        {
          ...
          Subsystems: [
            {
              ...
    	  Controllers: [
    	    {
                  "Controller":"nvme0",
    	      ...
                  "Namespaces":[
    	        {
                      "NameSpace":"nvme0n1",
    		  ...
    		}
    	      ]
    	    }
    	  ]
            }
          ]
        }
      ]
    }
    This fix ensures the JSON output is semantically accurate and easier to
    consume by tools that parse nvme list --verbose --output json.
    
    Reported-by: Maram Srimannarayana Murthy <msmurthy@imap.linux.ibm.com>
    Signed-off-by: Nilay Shroff <nilay@linux.ibm.com>
    Link: https://patch.msgid.link/20250721102946.1003295-1-nilay@linux.ibm.com
    Signed-off-by: Daniel Wagner <wagi@kernel.org>
    (cherry picked from commit 64bed0a87a23ca574cfce64fbcf6630194551276)
    Signed-off-by: Daniel Wagner <dwagner@suse.de>

--- nvme-print-json.c
+++ nvme-print-json.c
@@ -4295,9 +4295,6 @@ static void json_detail_list(nvme_root_t t)
 
 	nvme_host_t h;
 	nvme_subsystem_t s;
-	nvme_ctrl_t c;
-	nvme_path_t p;
-	nvme_ns_t n;
 
 	nvme_for_each_host(t, h) {
 		struct json_object *hss = json_create_object();
@@ -4311,76 +4308,14 @@ static void json_detail_list(nvme_root_t t)
 
 		nvme_for_each_subsystem(h, s) {
 			struct json_object *jss = json_create_object();
-			struct json_object *jctrls = json_create_array();
-			struct json_object *jnss = json_create_array();
 
 			obj_add_str(jss, "Subsystem", nvme_subsystem_get_name(s));
 			obj_add_str(jss, "SubsystemNQN", nvme_subsystem_get_nqn(s));
 
-			nvme_subsystem_for_each_ctrl(s, c) {
-				struct json_object *jctrl = json_create_object();
-				struct json_object *jnss = json_create_array();
-				struct json_object *jpaths = json_create_array();
-
-				obj_add_str(jctrl, "Controller", nvme_ctrl_get_name(c));
-				obj_add_str(jctrl, "Cntlid", nvme_ctrl_get_cntlid(c));
-				obj_add_str(jctrl, "SerialNumber", nvme_ctrl_get_serial(c));
-				obj_add_str(jctrl, "ModelNumber", nvme_ctrl_get_model(c));
-				obj_add_str(jctrl, "Firmware", nvme_ctrl_get_firmware(c));
-				obj_add_str(jctrl, "Transport", nvme_ctrl_get_transport(c));
-				obj_add_str(jctrl, "Address", nvme_ctrl_get_address(c));
-				obj_add_str(jctrl, "Slot", nvme_ctrl_get_phy_slot(c));
-
-				nvme_ctrl_for_each_ns(c, n) {
-					struct json_object *jns = json_create_object();
-					int lba = nvme_ns_get_lba_size(n);
-					uint64_t nsze = nvme_ns_get_lba_count(n) * lba;
-					uint64_t nuse = nvme_ns_get_lba_util(n) * lba;
-
-					obj_add_str(jns, "NameSpace", nvme_ns_get_name(n));
-					obj_add_str(jns, "Generic", nvme_ns_get_generic_name(n));
-					obj_add_int(jns, "NSID", nvme_ns_get_nsid(n));
-					obj_add_uint64(jns, "UsedBytes", nuse);
-					obj_add_uint64(jns, "MaximumLBA", nvme_ns_get_lba_count(n));
-					obj_add_uint64(jns, "PhysicalSize", nsze);
-					obj_add_int(jns, "SectorSize", lba);
-
-					array_add_obj(jnss, jns);
-				}
-				obj_add_obj(jctrl, "Namespaces", jnss);
-
-				nvme_ctrl_for_each_path(c, p) {
-					struct json_object *jpath = json_create_object();
-
-					obj_add_str(jpath, "Path", nvme_path_get_name(p));
-					obj_add_str(jpath, "ANAState", nvme_path_get_ana_state(p));
-
-					array_add_obj(jpaths, jpath);
-				}
-				obj_add_obj(jctrl, "Paths", jpaths);
-
-				array_add_obj(jctrls, jctrl);
-			}
-			obj_add_obj(jss, "Controllers", jctrls);
-
-			nvme_subsystem_for_each_ns(s, n) {
-				struct json_object *jns = json_create_object();
-
-				int lba = nvme_ns_get_lba_size(n);
-				uint64_t nsze = nvme_ns_get_lba_count(n) * lba;
-				uint64_t nuse = nvme_ns_get_lba_util(n) * lba;
-
-				obj_add_str(jns, "NameSpace", nvme_ns_get_name(n));
-				obj_add_str(jns, "Generic", nvme_ns_get_generic_name(n));
-				obj_add_int(jns, "NSID", nvme_ns_get_nsid(n));
-				obj_add_uint64(jns, "UsedBytes", nuse);
-				obj_add_uint64(jns, "MaximumLBA", nvme_ns_get_lba_count(n));
-				obj_add_uint64(jns, "PhysicalSize", nsze);
-				obj_add_int(jns, "SectorSize", lba);
-
-				array_add_obj(jnss, jns);
-			}
-			obj_add_obj(jss, "Namespaces", jnss);
+			if (nvme_is_multipath(s))
+				json_print_detail_list_multipath(s, jss);
+			else
+				json_print_detail_list(s, jss);
 
 			array_add_obj(jsslist, jss);
 		}
